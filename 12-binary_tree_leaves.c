ebaa96fb182995d6936ccea728eb9fabc84c6a43
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-05-29 16:04:52 +0100

    Solution for task 12. Leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count the leaves in a binary tree
+ *
+ * @tree: root of tree whose leaves are to be counted
+ *
+ * Return: 0 if tree is NULL ; otherwise return number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int leaf = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	leaf += binary_tree_leaves(tree->left);
+	leaf += binary_tree_leaves(tree->right);
+
+	if ((tree->left == NULL) || (tree->right == NULL))
+		return (1);
+
+	return (leaf);
+}
