261efd7d6cb900094b64ecf0fc52a80e5c1105f0
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-05-29 21:36:10 +0100

    Solution for task 14. Balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - find the balance factor of a binary tree node
+ *
+ * @tree: pointer to node to be checked
+ *
+ * Return: 2 if its a balanced node ; -1 if it has one child ; 0 otherwise
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	binary_tree_t *left_child;
+	binary_tree_t *right_child;
+
+	if (tree == NULL)
+		return (0);
+
+	left_child = tree->left;
+	right_child = tree->right;
+
+	if ((left_child != NULL) && (right_child != NULL))
+		return (2);
+	else if ((left_child != NULL) || (right_child != NULL))
+		return (-1);
+
+	return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
