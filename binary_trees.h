83cdf26d8f939ded0d7c5b0a26707bdac83335a1
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-05-30 09:29:11 +0100

    added all function prototypes to header file

--- binary_trees.h
+++ binary_trees.h
@@ -33,6 +33,7 @@ typedef struct binary_tree_s binary_tree_t;
 /*================== FUNCTION DEFINITIONS ===================*/
 /*===========================================================*/
 
+int count_nodes(binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_leaf(const binary_tree_t *node);
