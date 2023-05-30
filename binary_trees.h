f416571ff20fb4e9e5eeb3810da93fe0fe210cf4
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-05-30 09:32:59 +0100

    modified header file

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,10 @@
 
 #include <stddef.h>
 
+/*===========================================================*/
+/*========= DATA STRUCTURE TO BE USED (BINARY TREE) =========*/
+/*===========================================================*/
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,13 +15,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
 /*===========================================================*/
 /*====== DATA STRUCTURES WITH BASE OF <binary_tree_s> =======*/
