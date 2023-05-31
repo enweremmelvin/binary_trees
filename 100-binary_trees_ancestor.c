b13fd7ba1e7f62015a4631a879f77c15406bb4c1
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-05-31 07:28:46 +0100

    Solution for task 19. Lowest common ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,137 @@
+#include "binary_trees.h"
+
+/**
+ * look_up_second - search for the ancestor of given nodes by looking \
+ *                  upwards from second node
+ *
+ * @root: current root to be looked at
+ * @second: node to try matching underneath root
+ *
+ * Return: NULL if (second is null or no ancestor found) otherwise sec parent
+ */
+
+binary_tree_t *look_up_second(binary_tree_t *root, binary_tree_t *second)
+{
+	if (second == NULL)
+		return (NULL);
+
+	look_up_second(root, second->parent);
+
+	if ((root->left == second->parent) ||
+	    (root->right == second->parent))
+		return (second->parent);
+
+	return (NULL);
+}
+
+/**
+ * get_second_ancestor - find the second node under current root
+ *
+ * @root: current root to search for second in
+ * @second: second node to be searched for
+ *
+ * Return: NULL if second node not found ; otherwise return root
+ */
+
+binary_tree_t *get_second_ancestor(binary_tree_t *root, binary_tree_t *second)
+{
+	if ((root->left == NULL) && (root->right == NULL))
+		return (NULL);
+
+	if ((root->left == second) || (root->right == second))
+		return (root);
+
+	get_second_ancestor(root->left, second);
+	get_second_ancestor(root->right, second);
+
+	return (NULL);
+}
+
+
+/**
+ * get_ancestor - try to find the lowest common ancestor of node
+ *
+ * @first: first node
+ * @second: second node
+ * @ancestor: pointer to ancestor
+ * @n: number of times to iterate upwards a node's parent
+ *
+ * Return: 1 if ancestor found ; 0 otherwise
+ */
+
+int get_ancestor(const binary_tree_t *first,
+			    const binary_tree_t *second,
+			    binary_tree_t **ancestor, int n)
+{
+	int i;
+	binary_tree_t *temp, *sec;
+
+	if ((first == NULL) || (second == NULL) ||
+	    (first->parent == NULL))
+		return (0);
+
+	sec = second->parent;
+	temp = first->parent;
+
+	sec->left == second ? sec = sec->left : 1 * 0;
+	sec->right == second ? sec = sec->right : 1 * 0;
+	temp->left == first ? temp = temp->left : 1 * 0;
+	temp->right == first ? temp = temp->right : 1 * 0;
+
+	for (i = 0; ((i < n) && (temp != NULL)); i++)
+		temp = temp->parent;
+
+	if (temp != NULL)
+	{
+		/* search for second node under current parent */
+		if (get_second_ancestor(temp, sec) != NULL)
+		{
+			*ancestor = temp;
+			return (1);
+		}
+		else if (look_up_second(temp, sec) != NULL)
+		{
+			*ancestor = temp;
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+
+/**
+ * binary_trees_ancestor - find the lowest common ancestor
+ *                         of two nodes in a binary tree
+ *
+ * @first: first node
+ * @second: second node
+ *
+ * Return: pointer to lowest common ancestor of the two nodes \
+ *         or NULL if there is no common ancestor for the two given nodes
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	int i, ret_val;
+	binary_tree_t *ancestor = NULL;
+
+	if ((first == NULL) || (second == NULL))
+		return (NULL);
+
+	if (first == second->parent)
+		return (second->parent);
+	if (second == first->parent)
+		return (first->parent);
+
+	for (i = 0; i < 100; i++)
+	{
+		ret_val = get_ancestor(first, second, &ancestor, i);
+
+		if (ret_val == 1)
+			break;
+	}
+
+	return (ancestor);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -52,6 +52,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
