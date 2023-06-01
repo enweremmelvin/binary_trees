82cd85a0827cc1d91128916596338b2f3b2cb338
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-06-01 19:59:57 +0100

    correction for failing checks

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -56,6 +56,49 @@ void check_greatness(const binary_tree_t *tree,
 
 
 /**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void check_duplicate(const binary_tree_t *tree, int *index, int **ar)
+{
+	if (tree == NULL)
+		return;
+
+	(*ar)[*index] = tree->n;
+	*index += 1;
+
+	check_duplicate(tree->left, index, ar);
+	check_duplicate(tree->right, index, ar);
+}
+
+/**
+ * binary_tree_size - find the size of a binary tree
+ * --- size == all nodes inside the binary tree
+ *
+ * @tree: binary tree to be traversed
+ *
+ * Return: size of binary tree
+ */
+
+size_t tree_size(const binary_tree_t *tree)
+{
+	int size = 1;
+
+	if (tree == NULL)
+		return (0);
+
+	size += tree_size(tree->left);
+	size += tree_size(tree->right);
+
+	return (size);
+}
+
+
+/**
  * binary_tree_is_bst - check if a binary tree is a binary search tree
  *
  * @tree: binary tree to be checked
@@ -65,12 +108,32 @@ void check_greatness(const binary_tree_t *tree,
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int flag = 1;
+	int flag = 1, i, j, index = 0, *int_array = NULL;
 
 	if (tree == NULL)
 		return (0);
 
 	check_greatness(tree, tree, &flag);
+
+	if (flag == 1)
+	{
+		int_array = malloc(sizeof(int) * (tree_size(tree) + 1));
+		check_duplicate(tree, &index, &int_array);
+
+		for (i = 0; i < (int) tree_size(tree); i++)
+		{
+			for (j = 0; j < (int) tree_size(tree); j++)
+			{
+				if (i == j)
+					continue;
+
+				if (int_array[i] == int_array[j])
+					flag = 0;
+			}
+		}
+
+		free(int_array);
+	}
 
 	return (flag);
 }
