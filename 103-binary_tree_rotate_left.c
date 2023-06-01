425f69040cfcaee83ceba8136509c27b617856db
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-06-01 14:39:10 +0100

    Solution for task 22. Rotate left

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - rotate the nodes of a binary tree left
+ *
+ * @tree: tree whose nodes are to be rotated
+ *
+ * Return: new root of the binary tree
+ */
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *old_root;
+	binary_tree_t *new_root;
+	binary_tree_t *new_root_left;
+
+	if (tree == NULL)
+		return (NULL);
+	if (tree->rigt == NULL)
+		return (tree);
+
+	old_root = tree;
+	new_root = tree->right;
+	new_root_left = tree->right->left;
+
+	new_root->parent = NULL;
+	new_root->left = old_root;
+	old_root->parent = new_root;
+
+	old_root->right = new_root_left;
+
+	if (new_root_left != NULL)
+		new_root_left->parent = old_root;
+
+	tree = new_root;
+
+	return (tree);
+}
