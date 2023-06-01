2a0aa9f0ed2124257d31b0a5d18231b785f46b02
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-06-01 20:06:22 +0100

    correction for failing checks

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -17,7 +17,7 @@ void check_greatness(const binary_tree_t *tree,
 
 	if (tree->left != NULL)
 	{
-		if (tree->left->n > tree->n)
+		if (tree->left->n >= tree->n)
 		{
 			*flag = 0;
 			return;
@@ -25,7 +25,7 @@ void check_greatness(const binary_tree_t *tree,
 
 		if (tree->left->right != NULL)
 		{
-			if (tree->left->right->n > root->n)
+			if (tree->left->right->n >= root->n)
 			{
 				*flag = 0;
 				return;
@@ -35,14 +35,14 @@ void check_greatness(const binary_tree_t *tree,
 
 	if (tree->right != NULL)
 	{
-		if (tree->right->n < tree->n)
+		if (tree->right->n <= tree->n)
 		{
 			*flag = 0;
 			return;
 		}
 		if (tree->right->left != NULL)
 		{
-			if (tree->right->left->n < root->n)
+			if (tree->right->left->n <= root->n)
 			{
 				*flag = 0;
 				return;
@@ -56,49 +56,6 @@ void check_greatness(const binary_tree_t *tree,
 
 
 /**
- *
- *
- *
- *
- *
- */
-
-void check_duplicate(const binary_tree_t *tree, int *index, int **ar)
-{
-	if (tree == NULL)
-		return;
-
-	(*ar)[*index] = tree->n;
-	*index += 1;
-
-	check_duplicate(tree->left, index, ar);
-	check_duplicate(tree->right, index, ar);
-}
-
-/**
- * binary_tree_size - find the size of a binary tree
- * --- size == all nodes inside the binary tree
- *
- * @tree: binary tree to be traversed
- *
- * Return: size of binary tree
- */
-
-size_t tree_size(const binary_tree_t *tree)
-{
-	int size = 1;
-
-	if (tree == NULL)
-		return (0);
-
-	size += tree_size(tree->left);
-	size += tree_size(tree->right);
-
-	return (size);
-}
-
-
-/**
  * binary_tree_is_bst - check if a binary tree is a binary search tree
  *
  * @tree: binary tree to be checked
@@ -108,32 +65,12 @@ size_t tree_size(const binary_tree_t *tree)
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int flag = 1, i, j, index = 0, *int_array = NULL;
+	int flag = 1;
 
 	if (tree == NULL)
 		return (0);
 
 	check_greatness(tree, tree, &flag);
-
-	if (flag == 1)
-	{
-		int_array = malloc(sizeof(int) * (tree_size(tree) + 1));
-		check_duplicate(tree, &index, &int_array);
-
-		for (i = 0; i < (int) tree_size(tree); i++)
-		{
-			for (j = 0; j < (int) tree_size(tree); j++)
-			{
-				if (i == j)
-					continue;
-
-				if (int_array[i] == int_array[j])
-					flag = 0;
-			}
-		}
-
-		free(int_array);
-	}
 
 	return (flag);
 }
